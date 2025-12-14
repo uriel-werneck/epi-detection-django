314518d16dc0155133c3e86e61afa5d0dbc7bf35
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-14 17:20:52 -0300

    refactor(pages): remove unwanted routes

--- pages/urls.py
+++ pages/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-from .views import (
-    home,
-    login,
-    register
-)
+from .views import home
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register')
+    path('', home, name='home')
 ]
