ece4a97ca1ca0b23a0e15c2b45494192b0ec0f58
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-14 14:57:00 -0300

    chore: start pages app

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
