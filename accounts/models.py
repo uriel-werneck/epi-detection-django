f4ad503f6bdbb11f51034f33d34690c506ae356b
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-26 15:25:50 -0300

    feat(accounts): create custom User model from AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=120, unique=True)
+    email_confirmado = models.BooleanField(default=False)
+    # password field is inherited from AbstractUser
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'sobrenome', 'telefone']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.email} - {self.nome} {self.sobrenome}'
