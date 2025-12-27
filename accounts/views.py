560a94698ed3732d3f20a850086414b8c3f7b6f5
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-27 20:50:44 -0300

    feat(accounts): create register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = UserRegisterForm()
+        
+    return render(request, 'accounts/register.html', {'form': form})
