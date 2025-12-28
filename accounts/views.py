2df50d575872d35bad0b7bebc53b550828f68315
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-27 23:05:09 -0300

    feat(accounts): redirect authenticated user if access register route

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
+from .decorators import redirect_if_authenticated
 
 # Create your views here.
+@redirect_if_authenticated
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
