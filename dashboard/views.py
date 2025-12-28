ce6ffab4d5bd2f69930885afcd8749fcd82d6506
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-27 23:07:54 -0300

    feat(dashboard): redirect user to login page if not authenticated

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
     return render(request, 'dashboard/home.html')
 
+@login_required
 def upload(request, type):
     context = {'type': type}
 
@@ -12,12 +15,15 @@ def upload(request, type):
     elif type == 'upload-video':
         return render(request, 'dashboard/upload/upload-video.html', context)
 
+@login_required
 def minhas_deteccoes(request):
     return render(request, 'dashboard/minhas-deteccoes.html')
 
+@login_required
 def relatorios(request):
     return render(request, 'dashboard/relatorios.html')
 
+@login_required
 def get_detection_image(request, detection_id):
     context = {}
     return render(request, 'dashboard/minhas-deteccoes.html', context)
