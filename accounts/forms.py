d336fc18066119f751e308c5074f81b5ca995988
Uriel Werneck <uriel_werneck@hotmail.com>
2025-12-28 13:20:26 -0300

    feat(accounts): add clean methods to validate user input

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,15 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserRegisterForm(UserCreationForm):
+    def clean_nome(self):
+        cleaned_data = super().clean()
+        nome = cleaned_data.get('nome')
+        return nome.capitalize() if nome else nome
+
+    def clean_sobrenome(self):
+        sobrenome = self.cleaned_data.get('sobrenome')
+        return sobrenome.capitalize() if sobrenome else sobrenome
+
     class Meta:
         model = User
         fields = [
