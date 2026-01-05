ca40f90f46d45d8468c5cb920e6b289b630202a2
Uriel Werneck <uriel_werneck@hotmail.com>
2026-01-05 10:46:48 -0300

    feat(dashboard): use uuid4 as Detection id

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.utils import timezone
+import uuid
 
 # Create your models here.
 class Detection(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='detections')
     file_name = models.CharField(max_length=200, null=False, blank=False)
     detection_data = models.TextField(null=False, blank=False)
     timestamp = models.DateTimeField(default=timezone.now)
-    upload_type = models.CharField(max_length=20, null=False, blank=False)   # Alterado para suportar 'upload-video'
+    upload_type = models.CharField(max_length=20, null=False, blank=False)
     quantity = models.IntegerField(null=False, blank=False)
     detected_classes = models.TextField(null=False, blank=False)
-    # Novos campos para armazenar imagens e vídeos no banco de dados
     image_data = models.ImageField(upload_to='detections/images/', null=True, blank=True)
     video_data = models.FileField(upload_to='detections/videos/', null=True, blank=True)
-    is_stored_in_db = models.BooleanField(default=False) # Indica se está armazenado no banco
+    is_stored_in_db = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'detections'
