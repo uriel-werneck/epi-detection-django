843f98f61f28a5b967105a6fb348d3126480b5a2
Uriel Werneck <uriel_werneck@hotmail.com>
2026-01-07 23:25:43 -0300

    feat(dashboard): add route to download detection image result

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from .views import home, relatorios, minhas_deteccoes, upload, get_detection_image
+from .views import home, relatorios, minhas_deteccoes, upload, get_detection_image, download_image
 from django.conf.urls.static import static
 from django.conf import settings
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('relatorios/', relatorios, name='relatorios'),
-    path('minhas-deteccoes/', minhas_deteccoes, name='minhas_deteccoes'),
-    path('dashboard/upload/<str:type>/', upload, name='upload'),
-    path('get-detection-image/<int:detection_id>/', get_detection_image, name='get_detection_image')
+   path('home/', home, name='home'),
+   path('relatorios/', relatorios, name='relatorios'),
+   path('minhas-deteccoes/', minhas_deteccoes, name='minhas_deteccoes'),
+   path('dashboard/upload/<str:type>/', upload, name='upload'),
+   path('get-detection-image/<int:detection_id>/', get_detection_image, name='get_detection_image'),
+   path('dashboard/detections/<uuid:detection_id>/download/', download_image, name='download_imagem')
 ]
 
 if settings.DEBUG:
