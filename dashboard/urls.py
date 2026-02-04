4e5ed8f0171a2c13575acf029d31076cb6779793
Uriel Werneck <uriel_werneck@hotmail.com>
2026-02-04 20:35:11 -0300

    refactor(dashboard): move static files configuration to config

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from .views import home, relatorios, minhas_deteccoes, upload, get_detection_image, download_image
-from django.conf.urls.static import static
-from django.conf import settings
 
 app_name = 'dashboard'
 
@@ -13,9 +11,3 @@ urlpatterns = [
    path('get-detection-image/<int:detection_id>/', get_detection_image, name='get_detection_image'),
    path('dashboard/detections/<uuid:detection_id>/download/', download_image, name='download_imagem')
 ]
-
-if settings.DEBUG:
-   urlpatterns += static(
-      settings.MEDIA_URL,
-      document_root=settings.MEDIA_ROOT
-   ) 
